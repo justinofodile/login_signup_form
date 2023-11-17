c4cdfbd14c4bb70efdfb2adc6221dd2093013316
Ofodile Justin <justinofodile@gmail.com>
2023-11-17 10:03:13 +0100

    Another update

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,31 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
+import ActionBtn from "./components/ui/ActionBtn";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [display, setDisplay] = useState("Login");
+
+  function toggleDisplay(params) {
+    if (display === "Login") {
+      setDisplay("SignUp");
+    } else {
+      setDisplay("Login");
+    }
+  }
 
   return (
     <section className="flex justify-center h-screen space-x-6 items-center flex-col lg:flex-row">
-      <Login />
-      <SignUp />
+      {display === "Login" ? <Login /> : <SignUp />}
+
+      <button className="mt-5" onClick={toggleDisplay}>
+        Toogle Forms
+      </button>
+      <ActionBtn
+        actColor={"primary"}
+        text={"Toggle Form"}
+        onClick={toggleDisplay}
+      />
     </section>
   );
 }
